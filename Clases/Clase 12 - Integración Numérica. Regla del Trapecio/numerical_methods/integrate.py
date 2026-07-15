c5c9c79c9c9130e0d85523f2021ecc2920fb0152
Omar526 <omar.fernandez.o@usach.cl>
2026-07-15 13:05:42 -0400

    update clase_13

--- Clases/Clase 12 - Integración Numérica. Regla del Trapecio/numerical_methods/integrate.py
+++ Clases/Clase 12 - Integración Numérica. Regla del Trapecio/numerical_methods/integrate.py
@@ -4,24 +4,4 @@ def riemann_rule(f, a, b, n):
     pass
 
 def trapezoid_rule(f, a, b, n):
-    """ metodo para resolver integrales
-    f:
-     a:
-      b:
-       n:
-         """
-
-    dx = (b-a) / n # ancho de trapecio
-    xi = np.arange(a, b + dx, dx) # puntos del dominio
-    omega_i = dx * np.ones(len(xi))
-
-    # seleccionamos el primer elemento del array
-    omega_i[0] = dx/2
-
-    # seleccionamos el último elemento del array
-    omega_i[-1] = dx/2
-
-    Ai = omega_i * f(xi)
-    I = np.sum(Ai)
-
-    return I
+    pass
